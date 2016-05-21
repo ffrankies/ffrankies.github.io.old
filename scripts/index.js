b397cf60d300bf9ce99df45e11290bd45ae3dbc6
Frank <wanyefrank@live.com>
2016-05-21 12:57:58 -0400

    Index Background Animation
    Changed background animation of index page once more.
    Page does not load animation if width is below 480px.
    Animation is simple oscillating bars of varying colors
    CodePen version of background: http://codepen.io/ffrankies/pen/bpPddv

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,92 @@
 $(document).ready(function(){
 
+    //$(".body").hide();
+    // alert("hello");
+
+
     var anim = function() {
-        $(".orbits").empty();
+        $("#bars").empty();
 
         //Creates the 15 animated circles
-        var limit = 15;
+        var limit = 50;
 
         if($(window).width() < 800) {
-            limit = 8;
+            limit = 20;
         }
-        if($(window).width() < 480 || $(window).height() < 480) {
+
+        if($(window).width() < 480) {
             limit = 0;
         }
+
         for(i = 0; i < limit; ++i) {
-            $(".orbits").append("<div></div>");
-            //Circles increase in size
-            var usableHeight = $(window).height() - $("#header").height() - $("#footer").height();
-            var circlesize = Math.random() * usableHeight * 0.7;
-            //var fullHeight = $(document).height()*0.5;
-            var posw = $(document).width()*0.5 - ( ( circlesize ) / 2 );
-            var translate = ( ( Math.random() * 0.5 ) + 0.5 ) * -100;
-            var x = $(document).width() * 0.75 * ( Math.random() - 0.5 );
 
-            //alert(pos);
+            $("#bars").append("<div></div>");
 
-            //Circle color alternates
-            var color;
-            if( i % 4 == 0 ) {
-                color = "#FFE32D";
+            var x = i + 1;
+            var height = "25%";
+            var color = "#FFF68F";
+            var dur = "12s";
+            if( x == 2 || x == 12 || x == 22 || x == 32 || x == 42 ) {
+            color = "#c00000";
+            height = "38%";
+            dur = "15s";
             }
-            else if( i % 3 == 0 ) {
-                color = "#FF781D";
+            if( x == 3 || x == 13 || x == 23 || x == 33 || x == 43 ) {
+            color = "#FF7F00";
+            height = "46%";
+            dur = "9s";
             }
-            else if ( i % 2 == 0 ) {
-                color = "#9e0000";
-            } else {
-                color = "#cccccc";
+            if( x == 4 || x == 14 || x == 24 || x == 34 || x == 44 ) {
+            color = "#AF4035";
+            height = "36%";
+            dur = "20s";
             }
-            $(".orbits").children().eq(i).css({
-                "width": circlesize,
-                "height": circlesize,
-                // "top": posh,
-                "left": posw + x,
-                "background": color,
-                "z-index": 0 - i,
-                "animation-delay": i * 100,
-                "transform": "translateY("+translate+"%)"
+            if( x == 5 || x == 15 || x == 25 || x == 35 || x == 45 ) {
+            color = "#FF3300";
+            height = "24%";
+            dur = "17s";
+            }
+            if( x == 6 || x == 16 || x == 26 || x == 36 || x == 46 ) {
+            color = "#EECBAD";
+            height = "22%";
+            dur = "10s";
+            }
+            if( x == 7 || x == 17 || x == 27 || x == 37 || x == 47 ) {
+            color = "#EE0000";
+            height = "34%";
+            dur = "22s";
+            }
+            if( x == 8 || x == 18 || x == 28 || x == 38 || x == 48 ) {
+            color = "#FFEC8B";
+            height = "47%";
+            dur = "11s";
+            }
+            if( x == 9 || x == 19 || x == 29 || x == 39 || x == 49 ) {
+            color = "#B22222";
+            height = "35%";
+            dur = "18s";
+            }
+            if( x % 10 == 0 ) {
+            color = "#FFD39B";
+            height = "24%";
+            dur = "7s";
+            }
+            $("#bars").children().eq(i).css({
+            "height": height,
+            "left": i*2 + "%",
+            "background-color": color,
+            "animation-duration": dur
             });
-        }
 
+            if($(window).width() < 800) {
+                $("#bars").children().eq(i).css({
+                    "width": "3.5%",
+                    "left": i*5 + "%",
+                    "margin": "0% 0.75% 0% 0.75%"
+                });
+            }
+
+        }
     };
 
     //alert($(window).width());
