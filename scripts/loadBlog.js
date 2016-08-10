296fcd5a6031bdbcdbde532caae4600e2e2c1684
Frank <wanyefrank@live.com>
2016-08-10 22:02:49 +0000

    print statements for debugging
    added print statements for debugging

--- scripts/loadBlog.js
+++ scripts/loadBlog.js
@@ -49,7 +49,7 @@ var loadFile = function(path) {
     contents[contents.length] = newDiv;
     display(newDiv);
 
-}
+};
 
 /******************************************************************************
  * Displays the abbreviated contents of one blog post in a div with class
@@ -111,16 +111,24 @@ document.addEventListener("readystatechange", function() {
 
     if(document.getReadyState === "interactive") {
 
+        print("document is interactive");
+
         //Remove original "post" div from document - there should be only one
         var originalPost = document.getElementsByClassName("post")[0];
         var container = document.getElementById("container");
         container.removeChild("originalPost");
 
+        print("removed original post");
+
         makeLinks();
+
+        print("created links");
 
         //This should display a truncated version of each blogpost on the site
         for(var i = 0; i < links.length; ++i)
             loadFile(links[i]);
+
+        print("displayed short versions of posts");
 
         //Action listener to display full post instead of the short version
         //upon click
@@ -130,6 +138,8 @@ document.addEventListener("readystatechange", function() {
             posts[p].onclick = showMore(p);
 
         }
+
+        print("added action listeners");
 
     }
 
