15554ee011e9a0a0e9b03ba5cb44960a49ab2843
Frank <wanyefrank@live.com>
2016-08-30 15:04:26 -0400

    Screen resize  attempt #1
    - Resize image when screen size is changed (or orientation is changed)

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -29,112 +29,6 @@ var makeLinks = function() {
 
 };
 
-// /******************************************************************************
-//  * Fades an image out by reducing its opacity over time
-//  *****************************************************************************/
-// var fadeOut = function(element) {
-//
-//     var opacity = 1.00;
-//     element.style.opacity = opacity;
-//
-//     var timer = setInterval(function() {
-//
-//         if(opacity > 0) {
-//             opacity -= 0.05;
-//             element.style.opacity = opacity;
-//         }
-//         else
-//             clearInterval(timer);
-//
-//     }, 100); //Should take 2 seconds
-//
-//     element.style.display = "none";
-//
-// };
-//
-// /******************************************************************************
-//  * Fades an image in by increasing its opacity over time
-//  *****************************************************************************/
-// var fadeIn = function(element) {
-//
-//     var opacity = 0.00;
-//     element.style.opacity = opacity;
-//     element.style.display = "block";
-//     var timer = setInterval(function() {
-//
-//     if(opacity > 0) {
-//         opacity += 0.05;
-//         element.style.opacity = opacity;
-//     }
-//     else
-//         clearInterval(timer);
-//
-//     }, 100); //Should take 2 seconds
-//
-// };
-//
-// /******************************************************************************
-//  * Switches to next image in slideshow
-//  *****************************************************************************/
-// var nextImage = function() {
-//
-//     imageNumber++;
-//     if(imageNumber == links.length)
-//         imageNumber = 0;
-//
-//     var slide = document.getElementById("slide");
-//     var current = document.getElementById("current");
-//     // fadeOut(current);
-//     current.style.display = "none";
-//     slide.innerHTML = "<img id='current' src='" + links[imageNumber] + "'/>";
-//     if(current.style.width > slide.style.width)
-//         current.style.width = slide.style.width;
-//     if(current.style.height > slide.style.height)
-//         current.style.height = slide.style.height;
-//     current.style.display = "block";
-//     // fadeIn(current);
-//     // function fade(element) {
-//     // var op = 1;  // initial opacity
-//     // var timer = setInterval(function () {
-//     //     if (op <= 0.1){
-//     //         clearInterval(timer);
-//     //         element.style.display = 'none';
-//     //     }
-//     //     element.style.opacity = op;
-//     //     element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-//     //     op -= op * 0.1;
-//     // }, 50);
-// // }
-// };
-//
-// document.addEventListener("readystatechange", function() {
-//
-//     if(document.readyState === "interactive") {
-//
-//         console.log("document is interactive");
-//         makeLinks();
-//         var slide = document.getElementById("slide");
-//         slide.innerHTML = "<img id='current' src='" + links[0] + "'/>";
-//         var current = document.getElementById("current");
-//         // current.style.display = "none";
-//         if(current.style.width > slide.style.width)
-//             current.style.width = slide.style.width;
-//         if(current.style.height > slide.style.height)
-//             current.style.height = slide.style.height;
-//         current.style.display = "block";
-//         // fadeIn(current);
-//
-//         var timer = setInterval(function() {
-//
-//             nextImage();
-//
-//         }, 10 * 1000);
-//
-//     }
-//
-// });
-
-
 $(document).ready(function(){
     //Takes content of index file and pastes it into allli
     makeLinks();
@@ -165,6 +59,9 @@ $(document).ready(function(){
             nextImage();
         }, 10 * 1000);
 
+    /*
+     * Fades out current image, fades in next image from links array
+     */
     function nextImage() {
         k++;
         if (k == links.length)
@@ -182,18 +79,50 @@ $(document).ready(function(){
         }
         $("#current").css("max-width",cw);
         $("#current").css("max-height",ch);
-    }
+
+    };
+
+    /*
+     * Resizes image inside #slide
+     */
+    function changeSize() {
+
+        $("#current").hide();
+        cw = $("#slide").width();
+        cw = cw * 0.96;
+        ch = $("#slideshow").height();
+        ch -= 13;
+        $("#current").css("max-width", cw);
+        $("#current").css("max-height", ch);
+        $("#current").show();
+        $("#current").height(ch);
+        $("#current").width("auto");
+        var w = $("#current").width();
+        var h = $("#current").height();
+        if ( w > cw ) {
+            $("#current").width(cw);
+        }
+
+    };
 
     /*
      * Loads next image when slideshow is clicked on
      */
     $("#slide").click(function() {
+
         $("#current").stop();
         nextImage();
         clearInterval(show);
         show = setInterval(function() {
             nextImage();
         }, 10 * 1000);
+
     });
+
+    /*
+     * Changes size of picture when screen size changes
+     */
+     $(window).resize(changeSize);
+     $(window).on("orientationchange", changeSize);
 
 });
